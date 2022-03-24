7c73024f33905512d7604b38f3aef7ba4adde10e
Rowee Apor <roweeapor@gmail.com>
2022-03-24 21:07:17 +0800

    Page setup and basic navigation

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image="https://upload.wikimedia.org/wikipedia/commons/d/d6/London-Eye-2009.JPG"
+			title="A First Meetup"
+			address="Some street 5, Some City"
+			description="The meetup description"
+		/>
+	);
+}
+
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+	{
+		id: "m1",
+		title: "A First Meetup",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/d/d6/London-Eye-2009.JPG",
+		address: "Some address 5, 12345 Some City",
+		description: "This is a first meetup!",
+	},
+	{
+		id: "m2",
+		title: "A Second Meetup",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/c/c0/Giovanni_Antonio_Canal%2C_il_Canaletto_-_London_-_Westminster_Abbey%2C_with_a_Procession_of_Knights_of_the_Bath_-_WGA03950.jpg",
+		address: "Some address 10, 12345 Major City",
+		description: "This is a second meetup!",
+	},
+];
+
+function HomePage() {
+	return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
